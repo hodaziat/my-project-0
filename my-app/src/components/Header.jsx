ee8fe28a2bae9420730e142165a8f921f2239d87
hodaziat <hoda.ziat@fau.de>
2026-09-12 20:59:34 +0200

    add-heade--r

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div><h1 style={{color:"red"}}> welcome to my react app</h1></div>
+    <div><h1 style={{color:"red",textAlign:"center"}}> welcome to my react app</h1></div>
   )
 }
 
